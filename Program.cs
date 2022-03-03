2998796ab6537c3c6509d73094947cae0bd7519a
Rokas <rokamanauskas@gmail.com>
2022-03-03 18:25:53 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RestaurantModel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
